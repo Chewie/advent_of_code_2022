d97342c040bf9a6086e5bca823cd7daa8957f31f
Kévin Sztern <sztern.kevin@gmail.com>
2022-12-03 00:02:21 +0100

    day02: move part1 to specific module

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,11 +1,13 @@
 use std::io::Read;
 
+mod part1;
+
 fn main() {
     let mut lines = String::new();
     std::io::stdin().read_to_string(&mut lines).unwrap();
 
-    let strategy = day02::Strategy::from_string(lines);
+    let strategy = part1::Strategy::from_string(lines);
     let score = strategy.predict_score();
 
-    println!("Predicted score: {score}");
+    println!("Part 1 predicted score: {score}");
 }
