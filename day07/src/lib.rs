c43dbe8be7038406c4de061152ff4d374d61b2a0
Kévin Sztern <sztern.kevin@gmail.com>
2022-12-08 06:43:41 +0100

    day07: add part 2

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -27,7 +27,7 @@ impl FileSystem {
                 match &token[..2] {
                     "ls" => Self::handle_ls(Rc::clone(&current), token),
                     "cd" => current = Self::get_new_dir(current, token),
-                    _ => (), //unreachable!("unknown command"),
+                    _ => unreachable!("unknown command"),
                 };
             }
         }
@@ -35,15 +35,33 @@ impl FileSystem {
     }
 
     pub fn total_size_under_100k(&self) -> usize {
-        let mut good_dirs = Vec::new();
-        self.root.borrow().all_dirs_size(&mut good_dirs);
-        good_dirs.iter().sum()
+        let mut dirs = Vec::new();
+        self.root
+            .borrow()
+            .all_dirs_sizes_under_threshold(&mut dirs, 100000);
+        dirs.iter().sum()
+    }
+
+    pub fn smallest_dir_big_enough(&self) -> usize {
+        let mut dirs = Vec::new();
+        self.root
+            .borrow()
+            .all_dirs_sizes_under_threshold(&mut dirs, 70000000);
+
+        // By recursion, the outer dir is the last appended
+        let current_occupation = dirs.last().unwrap();
+
+        dirs.iter()
+            .copied()
+            .filter(|size| current_occupation - size < 40000000)
+            .min()
+            .unwrap()
     }
 
     fn handle_ls(current_dir: Rc<RefCell<DirEntry>>, token: &str) {
         // First token is the "ls" command, we skip to the output
         for line in token.lines().skip(1) {
-            let (left, right) = line.split_once(" ").unwrap();
+            let (left, right) = line.split_once(' ').unwrap();
             if left == "dir" {
                 current_dir.borrow_mut().subdirs.insert(
                     right.to_string(),
@@ -63,46 +81,37 @@ impl FileSystem {
     }
 
     fn get_new_dir(current_dir: Rc<RefCell<DirEntry>>, token: &str) -> Rc<RefCell<DirEntry>> {
-        let (_, arg) = token.trim().split_once(" ").unwrap();
+        let (_, arg) = token.trim().split_once(' ').unwrap();
         if arg == ".." {
             return current_dir.borrow().parent.upgrade().unwrap();
         } else {
             return Rc::clone(&current_dir.borrow().subdirs[arg]);
         }
     }
-
-    fn dump(&self, writer: &mut impl std::fmt::Write) {
-        self.root.borrow().dump(writer, "/", 0);
-    }
 }
 
 impl DirEntry {
-    fn dump(&self, writer: &mut impl std::fmt::Write, dir_name: &str, indent_size: usize) {
-        let indent = " ".repeat(indent_size);
-        write!(writer, "{indent}- {dir_name} (dir)\n").unwrap();
-        for (name, size) in &self.files {
-            write!(writer, "{indent}  - {name} (file, size={size})\n").unwrap();
-        }
-        for (child_name, child_entry) in &self.subdirs {
-            child_entry
-                .borrow()
-                .dump(writer, &child_name, indent_size + 2);
-        }
-    }
-
-    fn all_dirs_size(&self, mut good_dirs: &mut Vec<usize>) -> usize {
+    fn all_dirs_sizes_under_threshold(
+        &self,
+        good_dirs: &mut Vec<usize>,
+        threshold: usize,
+    ) -> usize {
         let files_size: usize = self.files.values().sum();
         let dirs_size: usize = self
             .subdirs
             .values()
-            .map(|subdir| subdir.borrow().all_dirs_size(&mut good_dirs))
+            .map(|subdir| {
+                subdir
+                    .borrow()
+                    .all_dirs_sizes_under_threshold(good_dirs, threshold)
+            })
             .sum();
 
         let total_size = files_size + dirs_size;
-        if total_size <= 100000 {
+        if total_size <= threshold {
             good_dirs.push(total_size);
         }
-        return total_size;
+        total_size
     }
 }
 
@@ -110,6 +119,27 @@ impl DirEntry {
 mod tests {
     use super::*;
     use indoc::indoc;
+
+    impl FileSystem {
+        fn dump(&self, writer: &mut impl std::fmt::Write) {
+            self.root.borrow().dump(writer, "/", 0);
+        }
+    }
+
+    impl DirEntry {
+        fn dump(&self, writer: &mut impl std::fmt::Write, dir_name: &str, indent_size: usize) {
+            let indent = " ".repeat(indent_size);
+            writeln!(writer, "{indent}- {dir_name} (dir)").unwrap();
+            for (name, size) in &self.files {
+                writeln!(writer, "{indent}  - {name} (file, size={size})").unwrap();
+            }
+            for (child_name, child_entry) in &self.subdirs {
+                child_entry
+                    .borrow()
+                    .dump(writer, &child_name, indent_size + 2);
+            }
+        }
+    }
 
     #[test]
     fn filesystem_from_string_empty() {
@@ -335,5 +365,42 @@ mod tests {
 
         // THEN
         assert_eq!(95437, total_size);
+    }
+
+    #[test]
+    fn filesystem_smallest_dir_big_enough() {
+        // GIVEN
+        let input = indoc! {"
+            $ cd /
+            $ ls
+            dir a
+            14848514 b.txt
+            8504156 c.dat
+            dir d
+            $ cd a
+            $ ls
+            dir e
+            29116 f
+            2557 g
+            62596 h.lst
+            $ cd e
+            $ ls
+            584 i
+            $ cd ..
+            $ cd ..
+            $ cd d
+            $ ls
+            4060174 j
+            8033020 d.log
+            5626152 d.ext
+            7214296 k
+            "};
+        let fs = FileSystem::from_string(input);
+
+        // WHEN
+        let total_size = fs.smallest_dir_big_enough();
+
+        // THEN
+        assert_eq!(24933642, total_size);
     }
 }
--- day07/src/main.rs
+++ day07/src/main.rs
@@ -5,7 +5,9 @@ fn main() {
     std::io::stdin().read_to_string(&mut input).unwrap();
 
     let fs = day07::FileSystem::from_string(&input);
-    let size = fs.total_size_under_100k();
+    let size_under_100k = fs.total_size_under_100k();
+    let smallest_dir = fs.smallest_dir_big_enough();
 
-    println!("Total size under 100k : {size}");
+    println!("Total size under 100k : {size_under_100k}");
+    println!("Size of smallest dir that can be removed: {smallest_dir}");
 }
